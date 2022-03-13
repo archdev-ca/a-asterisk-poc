baae5b865c53e0dd79dbf205a19b1eac8d3dda11
Archie G <acgelonga@gmail.com>
2022-03-12 22:22:56 -0600

    Add new obstacles

--- js/game.js
+++ js/game.js
@@ -5,6 +5,12 @@ engine.setStartNode(2, 2);
 engine.setEndNode(10, 10);
 engine.addObstacles([
   [2, 4],
+  [3, 4],
+  [4, 4],
+  [5, 4],
+  [6, 4],
+  [6, 3],
+  [6, 2],
   [9, 10],
   [9, 9],
   [10, 9],
